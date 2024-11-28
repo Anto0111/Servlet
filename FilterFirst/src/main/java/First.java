a60c1c78396902f60f223a0b9f7b53941ae38011
Anto_Anisten <143519879+Anto0111@users.noreply.github.com>
2024-11-28 15:29:47 +0530

    Add files via upload

---
+++ FilterFirst/src/main/java/First.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class First extends HttpServlet{
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String uname=req.getParameter("uname");
+		PrintWriter out = resp.getWriter();
+		out.println("Welcome"+uname);
+		
+		
+		
+		//super.doPost(req, resp);
+	}
+
+}
